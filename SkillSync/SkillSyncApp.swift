76ddbbabff10931c8b4bcddc3ff7bd6c267f5097
rutwij patil <patil.rutwij@gmail.com>
2023-08-06 20:20:18 +0530

    Initial Commit

---
+++ SkillSync/SkillSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillSyncApp.swift
+//  SkillSync
+//
+//  Created by Rutwij Patil on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SkillSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
